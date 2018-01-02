ad3828971e5416f65c8f6747ce860af03836c6cc
Isaac Watts <isaac@elikirk.com>
2018-01-02 10:27:44 -0700

    setup for c files

---
+++ 001/SoloLearn/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Please enter a number:" << endl;
+    int num;
+    cin >> num;
+    cout << "You entered:" << num;
+    //return 0;
+}
