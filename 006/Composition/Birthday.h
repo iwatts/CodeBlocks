35de86a60f2be555312845b414b3a707c6528b5e
Isaac Watts <isaac@elikirk.com>
2018-01-03 14:09:29 -0700

    fixing includes

--- 006/Composition/Birthday.h
+++ 006/Composition/Birthday.h
@@ -1,7 +1,6 @@
 #ifndef BIRTHDAY_H
 #define BIRTHDAY_H
-#include <iostream>
-#include <string>
+
 
 class Birthday
 {
--- 006/Composition/Person.h
+++ 006/Composition/Person.h
@@ -1,7 +1,6 @@
 #ifndef PERSON_H
 #define PERSON_H
 
-#include <iostream>
 #include <string>
 #include "Birthday.h"
 
