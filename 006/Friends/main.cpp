14228d3fc4090396fdfb50321e9e7167b1808141
Isaac Watts <isaac@elikirk.com>
2018-01-03 15:17:50 -0700

    friends stuff

---
+++ 006/Friends/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+class MyClass {
+    public:
+        MyClass() {
+            regVar = 0;
+        }
+    private:
+        int regVar;
+
+    friend void someFunc(MyClass &obj);
+};
+
+void someFunc(MyClass &obj) {
+    obj.regVar = 42;
+    cout << obj.regVar;
+}
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
